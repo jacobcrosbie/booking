33655b600d3fe4069509d41ed5ca90f780205424
jacobcrosbie <71030220+jacobcrosbie@users.noreply.github.com>
2022-01-13 11:58:19 +0000

    create, get, getAll, update, delete API's

--- booking/src/main/java/com/barber/booking/web/rest/HaircutResource.java
+++ booking/src/main/java/com/barber/booking/web/rest/HaircutResource.java
@@ -1,13 +1,10 @@
 package com.barber.booking.web.rest;
 
-import com.barber.booking.domain.Haircut;
 import com.barber.booking.exceptions.HaircutException;
-import com.barber.booking.repository.HaircutRepository;
 import com.barber.booking.service.HaircutService;
 import com.barber.booking.service.dto.HaircutRequest;
 import com.barber.booking.service.dto.HaircutResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +20,28 @@ public class HaircutResource {
 
     @PostMapping("/create")
     public ResponseEntity<HaircutResponse> createHaircut(@RequestBody HaircutRequest haircutRequest) throws HaircutException {
-        return new ResponseEntity<>(haircutService.createHaircut(haircutRequest), HttpStatus.OK);
-
+        return new ResponseEntity<>(haircutService.createHaircut(haircutRequest), HttpStatus.CREATED);
     }
+
+    @GetMapping("/details/{type}")
+    public ResponseEntity<HaircutResponse> getHaircutDetails(@PathVariable String type) throws HaircutException {
+        return new ResponseEntity<>(haircutService.getHaircutDetails(type), HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<HaircutResponse>> getAllHaircuts() {
+        return new ResponseEntity<>(haircutService.getAllHaircuts(), HttpStatus.OK);
+    }
+
+    @PutMapping("/details/update")
+    public ResponseEntity<HaircutResponse> updateHaircutDetails(@RequestBody HaircutRequest haircutRequest) throws HaircutException {
+        return new ResponseEntity<>(haircutService.updateHaircutDetails(haircutRequest),HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/delete/{type}")
+    public ResponseEntity<String> deleteHaircut(@PathVariable String type) throws HaircutException {
+        return new ResponseEntity<>(haircutService.deleteHaircut(type),HttpStatus.OK);
+    }
+
 }
